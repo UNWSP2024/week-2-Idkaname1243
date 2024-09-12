921c7acfd00dd60588481614a79ddfe8843f44d6
Idkaname1243 <anderskjelland7@gmail.com>
2024-09-12 08:59:56 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,5 +1,10 @@
 def personal_information():
-    print('...')
+    print('''
+    My name is Anders Kjelland
+    My address is 1280 county road J west Shoreview MN 55126
+    My phone Number is 651-895-9220
+    My college major is going to be accounting
+    ''')
 
 
 # Line which calls the above function.
