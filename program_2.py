f831921df1ac29bfc9dc16c90fb9880bbb7d34bf
Idkaname1243 <anderskjelland7@gmail.com>
2024-09-12 12:47:22 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,20 @@
-
 def average_age():
-    # Get User Input
+    you = int(input("write age here"))
+    f1 = int(input("write friend 1's age here"))
+    f2 = int(input("write friend 2's age here"))
+    f3 = int(input("write friend 3's age here"))
+    f4 = int(input("write friend 4's age here"))
+    avg = ((f1+f2+f3+f4+you)/5)
+    print("the average age of you and your friends is "+ str(avg))
+
+# Get User Input
 
 
-    # Sum ages
+# Sum ages
 
-    # Average the ages
+# Average the ages
 
-    # Print the results
+# Print the results
 
 # Line which calls the above function.
 average_age()
